d49b41a26f18c2f08b3aeeebe56e830d6a6da33f
Damon Smith <damon.easy.system@gmail.com>
2016-06-09 13:42:22 +0300

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+Easy System Exception
+=====================
+The package provides the interfaces of system exceptions.
+
+[![Build Status](https://travis-ci.org/easy-system/es-exception.svg?branch=master)](https://travis-ci.org/easy-system/es-exception)
+[![Coverage Status](https://coveralls.io/repos/github/easy-system/es-exception/badge.svg?branch=master)](https://coveralls.io/github/easy-system/es-exception?branch=master)
---
+++ src/NotFoundExceptionInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of the "Easy System" package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Damon Smith <damon.easy.system@gmail.com>
+ */
+namespace Es\Exception;
+
+/**
+ * The interface for exceptions that can be thrown due to the absence of the
+ * requested resource.
+ */
+interface NotFoundExceptionInterface extends ExceptionInterface
+{
+}
